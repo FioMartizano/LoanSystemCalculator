a2d16a4d2f572cbc5d230ca84cb45c0608cf58a6
FioMartizano <144452744+FioMartizano@users.noreply.github.com>
2025-11-27 15:39:15 +0800

    splash screen, added logo and bg

---
+++ app/src/main/java/com/example/loansystemcalculator/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.loansystemcalculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int SPLASH_DELAY = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, RegistrationActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_DELAY);
+    }
+}
+
